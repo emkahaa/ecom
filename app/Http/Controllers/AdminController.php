cbce0f9016b050b9b0576b2f4e162d2f5e8a8627
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2023-07-13 03:21:33 +0700

    INIT & TEMPLATE ADMIN

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    function loginHandler(Request $request) : string {
+        $fieldType = filter_var($request->login_id, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if ($fieldType == 'email') {
+            $request->validate([
+                'login_id' => 'required|email|exists:admins,email',
+                'password' => 'required|min:5|max:45'
+            ], [
+                'login_id.required' => 'Email or username is required',
+                'login_id.email' => 'Invalid email address',
+                'login_id.exists' => 'Email is not exist in system',
+                'password.required' => 'Password is required',
+            ]);
+        } else {
+            $request->validate([
+                'login_id' => 'required|exists:admins,username',
+                'password' => 'required|min:5|max:45'
+            ], [
+                'login_id.required' => 'Email or username is required',
+                'login_id.exists' => 'Email is not exist in system',
+                'password.required' => 'Password is required',
+            ]);
+        }
+        
+        $creds = array(
+            $fieldType => $request->login_id,
+            'password' => $request->password
+        );
+
+        if (Auth::guard('admin')->attempt($creds)) {
+            return redirect()->route('admin.home');
+        } else {
+            session()->flash('fail', 'Invalid Credentials');
+            return redirect()->route('admin.login');
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Admin $admin)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_07_05_125400_create_sellers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('is_active', ['2', '1', '0'])->default('0');
+            $table->string('payment_method')->nullable();
+            $table->string('payment_email')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sellers');
+    }
+};
